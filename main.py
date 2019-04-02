947b55c6f2bf875f61ee2c59ea7af62d4baaa9a8
yuy <yy_dgs@126.com>
2019-04-02 15:18:43 +0800

    first windows

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import cocos
+
+class Mylayer(cocos.layer.Layer):
+    def __init__(self):
+        super(Mylayer, self).__init__()
+        label = cocos.text.Label("forever nice",font_size = 36,anchor_x ="center",anchor_y="center")
+        label.position = (320,240)
+        self.add(label)
+
+
+if __name__ == '__main__':
+    director = cocos.director.director
+    director.init()
+    main_layer = Mylayer()
+    main_scene = cocos.scene.Scene(main_layer)
+    director.run(main_scene)
